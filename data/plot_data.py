a8293868c46001bb44f9b313845474eaab795740
volkancicek <volkanericlice@gmail.com>
2020-04-16 19:21:49 +0200

    rename plot label

--- data/plot_data.py
+++ data/plot_data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 def plot_results(predicted_data, true_data, path):
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
-    ax.plot(true_data, label='True Data')
+    ax.plot(true_data, label='Ground Truth')
     plt.plot(predicted_data, label='Prediction')
     plt.legend()
     plt.savefig(path)
@@ -14,7 +14,7 @@ def plot_results(predicted_data, true_data, path):
 def plot_results_multiple(predicted_data, true_data, prediction_len):
     fig = plt.figure(facecolor='white')
     ax = fig.add_subplot(111)
-    ax.plot(true_data, label='True Data')
+    ax.plot(true_data, label='Ground Truth')
     # Pad the list of predictions to shift it in the graph to it's correct start
     for i, data in enumerate(predicted_data):
         padding = [None for p in range(i * prediction_len)]
