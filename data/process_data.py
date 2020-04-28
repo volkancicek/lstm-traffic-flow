bfe81b5231baa705610908be355ef8f22b55e7c0
volkancicek <volkanericlice@gmail.com>
2020-04-28 20:52:17 +0200

    update normalize & add dates

--- data/process_data.py
+++ data/process_data.py
@@ -8,17 +8,19 @@ class ProcessData:
 
     def __init__(self, configs):
         self.configs = configs
-        self.columns = configs['data']['columns']
-        self.df = pd.read_csv(os.path.join('data', configs['data']['approach_1']['data_file_name']))
-        self.train_split = int(len(self.df) * configs['data']['train_test_split'])
+        df = pd.read_csv(os.path.join('data', configs['data']['approach_1']['data_file_name']))
+        self.train_split = int(len(df) * configs['data']['train_test_split'])
         self.target_col = configs['data']['target_column']
         self.feature_columns = configs['data']['feature_columns']
-        self.features = self.df.get(self.columns).values[:, self.feature_columns]
-        self.target = self.df.get(self.columns).values[:, self.target_col]
-        self.train_data_mean = self.df.get(self.columns).values[:self.train_split].mean(axis=0)
-        self.train_data_std = self.df.get(self.columns).values[:self.train_split].std(axis=0)
-        self.test_data_mean = self.df.get(self.columns).values[self.train_split:].mean(axis=0)
-        self.test_data_std = self.df.get(self.columns).values[self.train_split:].std(axis=0)
+        self.dates = df.get("date").values[:]
+        self.features = df.get(self.feature_columns).values[:]
+        self.target = df.get(self.target_col).values[:]
+        self.train_data_mean = self.features[:self.train_split].mean(axis=0)
+        self.train_data_std = self.features[:self.train_split].std(axis=0)
+        self.target_mean = self.target[:self.train_split].mean(axis=0)
+        self.target_std = self.target[:self.train_split].std(axis=0)
+
+
 
     def get_labeled_data(self, train_data=True, single_step=True):
         history_length = self.configs['data']['history_size']
@@ -26,6 +28,7 @@ class ProcessData:
         step = self.configs['data']['step']
         feature_list = []
         label_list = []
+        date_list = []
         if train_data:
             start_index = 0 + history_length
             end_index = self.train_split
@@ -39,17 +42,16 @@ class ProcessData:
 
             if single_step:
                 label_list.append(self.target[i + target_length])
+                date_list.append(self.dates[i + target_length])
             else:
                 label_list.append(self.target[i:i + target_length])
+                date_list.append(self.dates[i:i + target_length])
 
-        return np.array(feature_list), np.array(label_list)
+        return np.array(feature_list), np.array(label_list), date_list
 
     def normalize_data(self):
-        train_set = (self.df.get(self.columns).values[:self.train_split] - self.train_data_mean) / self.train_data_std
-        test_set = (self.df.get(self.columns).values[self.train_split:] - self.test_data_mean) / self.test_data_std
-        dataset = np.concatenate((train_set, test_set))
-        self.target = dataset[:, self.target_col]
-        self.features = dataset[:, self.feature_columns]
+        self.features = (self.features[:] - self.train_data_mean) / self.train_data_std
+        self.target = (self.target[:] - self.target_mean) / self.target_std
 
     def denormalize_target(self, y):
-        return y * self.test_data_std[self.target_col] + self.test_data_mean[self.target_col]
+        return (y * self.target_std) + self.target_mean
